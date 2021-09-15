3d8094e5d97b419db4baf366007d1751f3c03431
William <william@undefined.re>
2021-09-15 09:48:07 +0200

    roam completion: better than nothing ;)

--- config.el
+++ config.el
@@ -468,7 +468,8 @@
  ;; ignored or overwritten, doom rape.
  ;:i   "TAB"         #'company-indent-or-complete-common
  ;:i   [tab]         #'company-indent-or-complete-common
- :i   "C-i"         #'org-roam-node-insert
+ ;:i   "C-i"         #'org-roam-node-insert
+ :i   "C-i"         #'completion-at-point
  :i   "C-b"         #'yas-expand
  :i   "C-v"         #'evil-paste-before
  :i   "C-V"         #'evil-paste-before)
